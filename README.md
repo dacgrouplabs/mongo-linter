f634f314d697943dfea36973f507a271df4b7ea8
David Au <david.au@zeroflow.com>
2014-08-03 01:35:16 -0400

    Updated README.md and cleaned up mongo-linter.js a bit.

--- README.md
+++ README.md
@@ -1,9 +1,76 @@
+# mongo-linter
+
 [![Build Status](https://magnum.travis-ci.com/dacgrouplabs/mongo-linter.svg?token=vja1SanqcFPypny4gPcZ&branch=master)](https://magnum.travis-ci.com/dacgrouplabs/mongo-linter)
 
-Tool that connects to your mongodb database and runs jshint on all stored JavaScript.
+Tool that connects to your [mongodb](https://www.mongodb.org/) database and runs [jshint](http://www.jshint.com/docs/) on all stored JavaScript functions. Helps developers detect potential issues and avoid head-scratching trying to figure out which stored js function is causing db.loadServerScripts() to fail -- mongo shell error messages do not tell you which function is the culprit.
+
+## Installation
 
 ```
-npm install mongojs
-npm install jshint
+git clone https://github.com/dacgrouplabs/mongo-linter.git
+```
+
+Optionally, you can run `npm update` to grab the latest dependencies:
+```
+cd mongo-linter
+npm update
+```
+
+## Usage
+
+
+### Configure database connection string
+
+First, you will need to configure your connection settings in `config.json`. The contents look like:
+```
+{
+    "databaseUrl" : "username:password@serverip:port/database"
+}
+```
+Modify the `databaseUrl` value with your database connection info and credentials. mongo-linter will pass this connection string to the [mongojs](http://mafintosh.github.io/mongojs/) `connect` function like so:
+```javascript
+var db = require("mongojs").connect(databaseUrl, collections);
+```
+The connection string should follow the format described in [the mongo connection string docs](http://docs.mongodb.org/manual/reference/connection-string/).
+
+### Run mongo-linter
+
+```
 node .\mongo-linter.js
 ```
+
+Output example:
+
+```
+PS C:\dev\mongo-linter> node .\mongo-linter.js
+Connecting...
+2 errors in function aggregateAllTheThings
+
+
+Globals:
+    undefined
+    Array
+    Object
+-----------------------------------------
+Total errors: 2
+Suppressed errors: 2
+Remaining errors: 0
+PS C:\dev\mongo-linter>
+```
+
+
+## Roadmap
+
+0. Command line support for connection string as argument, fallback to config.json if no argument given
+1. Replace `mongojs` and use [mongodb](https://github.com/mongodb/node-mongodb-native/) native node driver instead
+2. Hack away at writing jshint support for mongodb stored js env
+3. Embed mongo-linter into shell or possibly hooked into mongodb system.js.save() event itself
+
+
+## License
+
+`mongo-linter`'s code in this repo uses the MIT license, see our LICENSE file.
+
+## Contributing
+
+Needs work
--- mongo-linter.js
+++ mongo-linter.js
@@ -1,19 +1,18 @@
-// npm install mongojs
-// npm install jshint
-
 // mongo-linter.js
-var config = require('./config.json')
-var databaseUrl = config.databaseUrl;
-var collections = ["users", "system.js"]
-console.log("Connecting...");
-var db = require("mongojs").connect(databaseUrl, collections);
+var config = require('./config.json');
 var jshint = require("jshint").JSHINT;
+var mongojs = require("mongojs");
+
+var databaseUrl = config.databaseUrl;
+var collections = ["users", "system.js"];
+console.log("Connecting...");
+var db = mongojs.connect(databaseUrl, collections);
 var totalErrors = 0;
 var suppressedErrors = 0;
 
-db.system.js.find({  }, { } , { /*limit: 10*/}, function(err, storedjsArray) {
+db.system.js.find({ }, { } , { /*limit: 10*/}, function(err, storedjsArray) {
   if( !storedjsArray) console.log("No stored JavaScript functions found");
-  else if (err ) console.log(err);
+  else if (err) console.log(err);
   else {
 //    console.log(storedjsArray.value.code);
 
@@ -24,9 +23,10 @@ db.system.js.find({  }, { } , { /*limit: 10*/}, function(err, storedjsArray) {
             "eqnull" : false,
             "laxbreak" : true,
             "sub" : true
-        }
-        var globals = {
-        }
+        };
+
+        var globals = {};
+
         if(jshint(storedjs.value.code.toString(), options, globals)) {
             console.log('Function ' + storedjs._id + ' has no errors.  Congrats!');
         } else {
@@ -39,13 +39,13 @@ db.system.js.find({  }, { } , { /*limit: 10*/}, function(err, storedjsArray) {
                 if (errors[j].code.substring(0, 1)=='W') {
                 // if (errors[j].code=='W025' ||
                     // errors[j].code=='W041') {
-                    
+
                     suppressedErrors++;
                 }
                 else {
                     console.log(errors[j].line + ':' + errors[j].character + ' (' + errors[j].code + ') -> ' + errors[j].reason + ' -> ' + errors[j].evidence);
                 }
-                                
+
                 totalErrors++;
             }
 
@@ -54,15 +54,16 @@ db.system.js.find({  }, { } , { /*limit: 10*/}, function(err, storedjsArray) {
             console.log('Globals: ');
             for(j=0;j<out.globals.length;j++) {
                 console.log('    ' + out.globals[j]);
-            }   
+            }
         }
         console.log('-----------------------------------------');
       } );
-      
+
       console.log('Total errors: ' + totalErrors);
       console.log('Suppressed errors: ' + suppressedErrors);
       console.log('Remaining errors: ' + (totalErrors - suppressedErrors));
   }
-  
+
+  db.close();
   process.exit();
 });
